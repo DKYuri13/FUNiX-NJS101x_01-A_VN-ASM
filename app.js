09dc115027a57f86d158eab25eb27d13914882ef
Hoang Duy Quang <quang9514@gmail.com>
2022-06-28 16:51:09 +0700

    lab1.3

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 function rqListener(req, res) {
     console.log(req);
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 };
 
 const server = http.createServer(rqListener);
