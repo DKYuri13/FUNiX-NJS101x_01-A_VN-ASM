9994a4971000c6149ca02ab6556fecb249fb708e
Hoang Duy Quang <quang9514@gmail.com>
2022-07-05 11:57:56 +0700

    lab2.7-2

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
-const router = express.Router();
 const express = require('express');
+const router = express.Router();
 
 router.get('/add-product',(req, res, next)=>{
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
