73b2ca612e620a5cde3f094ba944fc356b4edac1
Hoang Duy Quang <quang9514@gmail.com>
2022-07-12 11:17:45 +0700

    lab4.14

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart')
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -14,7 +14,11 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail', {product: product, pageTitle: product.title, path: '/products'});
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   })
 }
 
