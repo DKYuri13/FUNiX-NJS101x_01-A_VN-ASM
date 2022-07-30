b0755356a2c94f7ed93d087349dd670e0f39406e
Hoang Duy Quang <quang9514@gmail.com>
2022-07-30 18:38:12 +0700

    lab6.2

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const MongoClient = mongodb.MongoClient;
 const mongoConnect = callback => {
     MongoClient
         .connect('mongodb+srv://Yuri141:Quang013.@cluster0.eib6oef.mongodb.net/?retryWrites=true&w=majority')
-        .then(result => {
+        .then(client => {
         console.log('Connected!');
         callback(client);
         })
