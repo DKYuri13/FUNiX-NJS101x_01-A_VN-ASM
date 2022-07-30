9e7740b3eb244c2425caac0238f4134755891203
Hoang Duy Quang <quang9514@gmail.com>
2022-07-30 18:37:45 +0700

    lab6.2

--- util/database.js
+++ util/database.js
@@ -2,13 +2,15 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
-
-MongoClient.connect('mongodb+srv://Yuri141:Quang013.@cluster0.eib6oef.mongodb.net/?retryWrites=true&w=majority')
-.then(result => {
-    console.log('Connected!');
-    callback(result);
-})
-.catch(err => console.log(err));
+    MongoClient
+        .connect('mongodb+srv://Yuri141:Quang013.@cluster0.eib6oef.mongodb.net/?retryWrites=true&w=majority')
+        .then(result => {
+        console.log('Connected!');
+        callback(client);
+        })
+        .catch(err => {
+        console.log(err)
+        });
 };
 
 module.exports = mongoConnect;
