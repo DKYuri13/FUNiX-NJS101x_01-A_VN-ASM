a42fe3d1374fb1c48b3462a570bc70f578bd9004
Hoang Duy Quang <quang9514@gmail.com>
2022-08-02 11:05:19 +0700

    lab6.12

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price,
     this.description = description,
     this.imageUrl = imageUrl,
-    this._id = new mongodb.ObjectId(id)
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
@@ -53,7 +53,7 @@ class Product {
   }
 
   static deleteById(prodId) {
-    const db = getDb;
+    const db = getDb();
     return db.collection('products')
       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
       .then(result => {
