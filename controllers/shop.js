9511900611024e016d4a01d14c8c2f4aab5d6d3d
Hoang Duy Quang <quang9514@gmail.com>
2022-08-10 18:09:32 +0700

    lab7.11

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,7 +58,8 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId).then(product => {
     return req.user.addToCart(product);
-  }).then(result => {
+  })
+  .then(result => {
     console.log(result);
     res.redirect('/cart');
   });
