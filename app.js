a150b6f712951619d660e9cb4aa4ba92e4ca9025
Hoang Duy Quang <quang9514@gmail.com>
2022-08-10 17:24:41 +0700

    lab7.3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('62e8a4ed351f98de65536ab3')
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById('62e8a4ed351f98de65536ab3')
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,7 +33,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect('mongodb+srv://Yuri141:Quang013.@cluster0.eib6oef.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://Yuri141:Quang013.@cluster0.eib6oef.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
         app.listen(3000);
     })
