780d138f53541546419c5217126230cd092d138d
Hoang Duy Quang <quang9514@gmail.com>
2022-08-10 16:51:10 +0700

    lab6.23

--- models/user.js
+++ models/user.js
@@ -74,17 +74,31 @@ class User {
 
     addOrder() {
         const db = getDb();
-        return db.collection('orders')
-            .insertOne(this.cart)
-            .then(result => {
-                this.cart = { items: [] };
-                db
-                .collection('users')
-                .updateOne(
-                    { _id: new ObjectId(this._id) },
-                    { $set: { cart: { items: [] } } }
-                );
-            });
+        return this.getCart()
+                .then(products => {
+                    const order = {
+                        items: products,
+                        user: {
+                            _id: new ObjectId(this._id),
+                            name: this.name
+                        }
+                    };
+                    return db.collection('orders').insertOne(order);
+                })
+                .then(result => {
+                    this.cart = { items: [] };
+                    db
+                    .collection('users')
+                    .updateOne(
+                        { _id: new ObjectId(this._id) },
+                        { $set: { cart: { items: [] } } }
+                    );
+                });
+    }
+
+    getOrder() {
+        const db = getDb();
+        // return db.collection('orders').
     }
 
     static findById(userId) { 
