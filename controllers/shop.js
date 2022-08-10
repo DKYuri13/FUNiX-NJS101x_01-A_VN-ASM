ece852e88ca2af04d8ad7330b364e4d5d4fe8f86
Hoang Duy Quang <quang9514@gmail.com>
2022-08-10 18:50:42 +0700

    lab7.17

--- controllers/shop.js
+++ controllers/shop.js
@@ -103,14 +103,13 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user
-  .getOrders({include: ['products']})
-  .then(orders => {
-    res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-    orders: orders
-    });
-  })
-  .catch(err => console.log(err));
+  Order.find({ "user.userId": req.user._id })
+    .then(orders => {
+      res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+      });
+    })
+    .catch(err => console.log(err));
 };
