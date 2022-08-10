92b38300561a1304de6274a53ffc64fc48b4a78c
Hoang Duy Quang <quang9514@gmail.com>
2022-08-10 17:38:56 +0700

    lab7.6

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,7 +37,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-  // Product.findByPk(prodId)
   .then(product => {
     if (!product) {
       return res.redirect('/');
@@ -58,15 +57,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl, 
-    prodId
-  );
-  product
-    .save()
+
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product
+        .save()
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -75,7 +75,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
