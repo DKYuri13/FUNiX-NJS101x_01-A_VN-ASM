5cfb102597b50f55088f436c964c36e01a899a9c
Hoang Duy Quang <quang9514@gmail.com>
2022-08-10 16:40:10 +0700

    lab6.22

--- models/user.js
+++ models/user.js
@@ -72,6 +72,21 @@ class User {
             );
     }
 
+    addOrder() {
+        const db = getDb();
+        return db.collection('orders')
+            .insertOne(this.cart)
+            .then(result => {
+                this.cart = { items: [] };
+                db
+                .collection('users')
+                .updateOne(
+                    { _id: new ObjectId(this._id) },
+                    { $set: { cart: { items: [] } } }
+                );
+            });
+    }
+
     static findById(userId) { 
         const db = getDb();
         return db
