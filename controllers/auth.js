fb2acb06b1d7de217cdf8ec63711343e0a8eb112
Hoang Duy Quang <quang9514@gmail.com>
2022-08-22 14:55:59 +0700

    lab8.2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,17 @@
 exports.getLogin = (req, res, next) => {
-        res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
-        });
+    const isLoggedIn = req
+        .get('Cookie')
+        .split(';')[0]
+        .trim()
+        .split('=')[1] === 'true';
+    res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+    });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/');
 };
