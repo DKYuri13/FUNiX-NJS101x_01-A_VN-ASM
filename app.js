ec808a23c4edb2f1da59969ed22a9959f80dab4b
Hoang Duy Quang <quang9514@gmail.com>
2022-08-25 17:38:42 +0700

    lab9.19

--- app.js
+++ app.js
@@ -41,12 +41,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
-      // throw new Error('Sync Dummy');
       if (!user) {
         return next();
       }
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err);
+      next(new Error(err));
     });
 });
 
@@ -74,7 +74,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render()
-  res.redirect('/500')
+  // res.redirect('/500')
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 })
 
 mongoose
