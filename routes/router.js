430bb912de035f692b600fc58b3083ee5f81d688
Hoang Duy Quang <quang9514@gmail.com>
2022-09-13 16:08:49 +0700

    more cmt

--- routes/router.js
+++ routes/router.js
@@ -12,22 +12,22 @@ router.post('/startWork', staffController.postRollCall); //Router checkin
 
 router.post('/endWork', staffController.postStopWork); //Router checkout
 
-router.post('/annualLeave', staffController.postAnnualLeave);
+router.post('/annualLeave', staffController.postAnnualLeave);   //Router xin nghỉ
 
-router.get('/information', staffController.getInformation);
+router.get('/information', staffController.getInformation); //Router hiển thị trang thông tin cá nhân
 
-router.get('/work-history', staffController.getWorkHistory);
+router.get('/work-history', staffController.getWorkHistory);    //Router hiển thị trang lịch sử làm việc
 
-router.post('/work-history/', staffController.postMonthWorkHistory);
+router.post('/work-history/', staffController.postMonthWorkHistory);    //Router chọn tháng lương hiển thị (trang lịch sử làm việc)
 
-router.get('/covid-info', staffController.getCovidInfo);
+router.get('/covid-info', staffController.getCovidInfo);    //Router hiển thị trang đăng ký thông tin covid
 
-router.post('/covid-temperature', staffController.postCovidTemperature);
+router.post('/covid-temperature', staffController.postCovidTemperature);    //Router đăng ký thân nhiệt
 
-router.post('/covid-vaccine', staffController.postCovidVaccine);
+router.post('/covid-vaccine', staffController.postCovidVaccine);    //Router đăng ký thông tin tiêm vaccine
 
-router.post('/covid-status', staffController.postCovidStatus);
+router.post('/covid-status', staffController.postCovidStatus);  //Router đăng ký tình trạng covid
 
-router.post('/editImage', staffController.postImageUrl);
+router.post('/editImage', staffController.postImageUrl);    //Router sửa ảnh
 
 module.exports = router;
